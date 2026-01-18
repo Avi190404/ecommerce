3467156f13466edccac0625d16d2ef17b3cc6a61
Avi <avi19042004@gmail.com>
2026-01-18 21:23:05 +0530

    Update Cart Schema

--- models/cartModel.ts
+++ models/cartModel.ts
@@ -10,18 +10,30 @@ const cartSchema = new mongoose.Schema({
         quantity: {
             type: Number,
             required: true,
+        },
+        amount: {
+            type: Number,
+            required: true,
         }
     }],
     user: {
         type: mongoose.Schema.ObjectId,
         ref: "User",
-        required: true
+        required: true,
+        index: true,
+        unique: true
     },
     totalAmount: {
         type: Number,
         default: 0
     }
 }, { timestamps: true } )
+
+cartSchema.pre('save', async function() {
+    this.totalAmount = this.item.reduce((total, i) => {
+        return total + (i.amount * i.quantity);
+    }, 0);
+});
 
 const CART = mongoose.models.Cart || mongoose.model("Cart", cartSchema);
 export default CART;
